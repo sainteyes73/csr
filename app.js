7574d1370c46a83917cdaab57f2b7b2f97b0cd38
unknown <a1903009@amotech.co.kr>
2019-05-20 11:47:17 +0900

    login

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ var cookieParser=require('cookie-parser');
 var path=require('path');
 var select=require('./routes/list.js')
 var sassMiddleware = require('node-sass-middleware');
+var passport=require('passport')
+var passportConfig = require('./lib/passport-config');
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'pug');
 app.use('/', index);
-app.use('/select', select)
+app.use('/select', select);
+
 app.use(cookieParser());
 // public 디렉토리에 있는 내용은 static하게 service하도록.
 app.use(express.static(path.join(__dirname, '/public')));
@@ -19,8 +22,12 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
+console.log('ok')
+app.use(passport.initialize());
+app.use(passport.session());
+passportConfig(passport);
 // error handler
+require('./routes/auth')(passport);
 
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -35,3 +42,5 @@ app.use(function(err, req, res, next) {
 app.listen(8080,()=>{
   console.log('express app on port 8080!');
 });
+
+module.exports=app;
