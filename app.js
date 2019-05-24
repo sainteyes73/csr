faf51baad9abbfbff57f1e0e9b4d9243590e0b80
unknown <a1903009@amotech.co.kr>
2019-05-24 16:25:41 +0900

    passport-config:수정중

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var index = require('./routes/index.js');
 var engines = require('consolidate');
-var app=express();
 var cookieParser=require('cookie-parser');
 var path=require('path');
 var select=require('./routes/list.js')
@@ -9,51 +8,57 @@ var sassMiddleware = require('node-sass-middleware');
 var passport=require('passport')
 var passportConfig = require('./lib/passport-config');
 var flash = require('connect-flash');
+var logger = require('morgan');
 var session  = require('express-session');
 
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine', 'pug');
+module.exports=(app,io)=>{
+  app.set('views',path.join(__dirname,'views'));
+  app.set('view engine', 'pug');
+  app.use(express.static(path.join(__dirname, '/public')));
+  app.use(logger('dev'));
+  app.use(cookieParser());
+  app.use(flash());
+  app.use(session({
+    secret:'afasdwqsdasdasd',
+    resave: false,
+    saveUninitialized: true
+  }));
 
-app.use('/', index);
-app.use('/select', select);
-app.use(cookieParser());
-app.use(session({
-  secret:'afasdwqsdasdasd',
-  resave: false,
-  saveUninitialized: true
-}));
-// public 디렉토리에 있는 내용은 static하게 service하도록.
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passportConfig(passport);
+  app.use('/', index);
+  require('./routes/auth')(app, passport);
+  app.use('/select', select);
 
-app.use(flash()); // flash message를 사용할 수 있도록
-app.use(passport.initialize());
-app.use(passport.session());
-passportConfig(passport);
 
-app.use(function(req, res, next) {
-  res.locals.currentUser = req.user;  // passport는 req.user로 user정보 전달
-  res.locals.flashMessages = req.flash();
-  next();
-});
-// error handler
 
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // public 디렉토리에 있는 내용은 static하게 service하도록.
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-app.listen(8080,()=>{
-  console.log('express app on port 8080!');
-});
 
-module.exports=app;
+
+  app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;  // passport는 req.user로 user정보 전달
+    res.locals.flashMessages = req.flash();
+    next();
+  });
+  // error handler
+
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  return app;
+}
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ function needAuth(req, res, next) {
   if (req.isAuthenticated()) {
     next();
   } else {
-  //  req.flash('danger', 'Please signin first.');
+    req.flash('danger', 'Please signin first.');
     res.redirect('signin');
   }
 }
@@ -15,26 +15,6 @@ function needAuth(req, res, next) {
 router.get('/', needAuth, catchErrors(async(req,res,next)=>{
   res.render('index');
 }));
-
-router.get('/signin', (req, res, next) => {
-  res.render('signin');
-});
-
-router.get('/list',(req,res,next)=>{
-  res.render('list');
-});
-
-router.post('/signin', passport.authenticate('local-signin', {
-  successRedirect: '/questions', // redirect to the secure profile section, 이때는 routes에 index.js를 가져온다
-  failureRedirect: '/signin', // redirect back to the signup page if there is an error
-  failureFlash: true // allow flash messages
-}));
-
-router.get('/signout', (req, res) => {
-  req.logout();
-  //req.flash('success', 'Successfully signed out');
-  res.redirect('/');
-});
 
 
 module.exports=router;
